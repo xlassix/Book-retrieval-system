5e83a9782101916ed027c80db19cd09d26265c55
OKI Ayobami <46370698+xlassix@users.noreply.github.com>
2020-01-08 15:45:05 +0000

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku,dj_database_url 
+import django_heroku 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
 ]
 #MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
-django_heroku.settings(locals())
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+django_heroku.settings(locals())
