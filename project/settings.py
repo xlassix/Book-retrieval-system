dbfd49c1697df32b4998ef0544b4d59b91fee397
OKI Ayobami <46370698+xlassix@users.noreply.github.com>
2020-01-08 15:38:16 +0000

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+import django_heroku,dj_database_url 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for productio
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -148,3 +148,6 @@ STATICFILES_DIRS = [
 #MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
 django_heroku.settings(locals())
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
