14f701ba07f7a39109346e01e2fee1fb8fc49e32
OKI Ayobami <46370698+xlassix@users.noreply.github.com>
2020-01-09 08:11:30 +0000

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -135,9 +135,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),os.path.join(BASE_DIR,'project_name/static')
+    os.path.join(BASE_DIR, "static"),
 ]
-
 
 #MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 db_from_env = dj_database_url.config(conn_max_age=600,ssl_require=True)
