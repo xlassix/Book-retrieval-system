1c01b9eec614f45b5ea3e0a356df33c2b53b7a6e
OKI Ayobami <46370698+xlassix@users.noreply.github.com>
2020-01-10 10:59:01 +0000

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -161,5 +161,5 @@ MESSAGE_TAGS = {
 
 django_heroku.settings(locals())
 
-db_from_env = dj_database_url.config(conn_max_age=600,ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
